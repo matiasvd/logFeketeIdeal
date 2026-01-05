3330899a00de26a497ffaac52b6af5ca11ad48ee
usuario <usuario@rosa.rosa>
2026-01-05 00:09:46 -0300

    Cartesian formulation folder added, together with the respective tests for seven points with dipole.

---
+++ idealCartesian/executeClusteruy.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH --job-name=CartesianDipole
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=200G
+#SBATCH --time=20:00:00
+#SBATCH --tmp=10G
+#SBATCH --partition=normal
+#SBATCH --qos=normal
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mvaldes@fing.edu.uy
+
+threads=40
+# maxPairs=1000
+
+# input file
+input=n7p0idealCartesianDipole.ms
+
+# output file
+output=n7p0idealCartesianDipoleBasis.ms
+
+# code directory
+dirCode=~/idealCartesian/
+cd $dirCode
+
+# path to msolve (relative to dirCode)
+# binMsolve=~/msolve094amdAVX512
+binMsolve=~/msolve094IntelAVX512
+# binMsolve=~/msolve094Intel
+
+source /etc/profile.d/modules.sh
+
+# reduced Grobner basis with msolve
+
+# with avx512 instructions (binary from msolve webpage)
+# submit execution with: sbatch --exclude=node31,node40,node41,node42,node43,node44,node45,node46,node47 executeClusteruy.sh
+
+# without avx512 instructions (binary from msvole webpage)
+# submit execution with: sbatch --nodelist=node41 executeClusteruy.sh
+
+#####
+# save all the monomials of the basis, not only the leading terms
+#####
+$binMsolve -g 2 -v 2 -t $threads -f $input -o $output
+
+#####
+# eliminate first m variables
+#####
+# $binMsolve -g 2 -v 2 -e 7 -t $threads -f $input -o $output
+
+#####
+# save only the leading monomials of the final basis (smaller file)
+#####
+# $binMsolve -g 1 -v 2 -t $threads -f $input -o $output
+
+
+# show job stats
+sstat -j $SLURM_JOB_ID.batch --format=JobID,MaxPages,MaxRSS,MaxVMSize,NTasks,NodeList
+
